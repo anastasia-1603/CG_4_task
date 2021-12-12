d812e501df2e7f57b259965a3b315f424c0c12f5
Anastasia Lazutkina <lazutkina.anastasia@mail.ru>
2021-12-12 23:51:48 +0300

    math part complete

---
+++ src/math/Matrix4.java
@@ -0,0 +1,123 @@
+package math;
+
+public class Matrix4 {
+    private float matrix[];
+    public static final int SIZE = 4;
+
+    public Matrix4(float[][] m) {
+        matrix = new float[16];
+        for (int i = 0; i < SIZE; i++) {
+            for (int j = 0; j < SIZE; j++) {
+                matrix[i * SIZE + j] = m[i][j];
+            }
+        }
+    }
+
+    /**
+     * Возвращает значение матрицы по заданным индексам
+     *
+     * @param row номер строки
+     * @param col номер столбца
+     * @return
+     */
+    public float getAt(int row, int col) {
+        return matrix[row * SIZE + col];
+    }
+
+    /**
+     * Задает значение матрицы по заданным индексам
+     *
+     * @param row   номер строки
+     * @param col   номер столбца
+     * @param value новое значение
+     * @return
+     */
+    public void setAt(int row, int col, float value) {
+        matrix[row * SIZE + col] = value;
+    }
+
+    private Matrix4(float[] arr) {
+        matrix = arr;
+    }
+
+
+    /**
+     * Создает новую матрицу, заполненную нулями
+     *
+     * @return нулевая матрица
+     */
+    public static Matrix4 zero() {
+        return new Matrix4(new float[16]);
+    }
+
+    /**
+     * Создает единичную матрицу
+     *
+     * @return единичная матрица
+     */
+    public static Matrix4 one() {
+        Matrix4 m = zero();
+        for (int i = 0; i < SIZE; i++) {
+            m.setAt(i, i, 1);
+        }
+        return m;
+    }
+
+
+
+    public Matrix4 multiply(float number)
+    {
+        Matrix4 m = zero();
+        for (int i = 0; i < this.matrix.length; i++)
+        {
+            m.matrix[i] = this.matrix[i] * number;
+        }
+        return m;
+    }
+
+    public Vector4 multiply(Vector4 vector)
+    {
+        float[] result = new float[4];
+        for (int i = 0; i < SIZE; i++)
+        {
+            float sum = 0;
+            for (int j = 0; j < SIZE; j++)
+            {
+                sum += this.getAt(i, j) * vector.at(j);
+            }
+            result[i] = sum;
+        }
+        return new Vector4(result);
+    }
+
+    public Matrix4 multiply(Matrix4 other)
+    {
+        Matrix4 result = zero();
+        for (int i = 0; i < SIZE; i++)
+        {
+            for (int j = 0; j < SIZE; j++)
+            {
+                float sum = 0;
+                for (int k = 0; k < SIZE; k++)
+                {
+                    sum += this.getAt(i, k) * other.getAt(k, j);
+                }
+                result.setAt(i, j, sum);
+            }
+        }
+        return result;
+    }
+
+    public Matrix4 add  (Matrix4 other)
+    {
+        Matrix4 result = Matrix4.zero();
+        for (int i = 0; i < SIZE; i++)
+        {
+            for (int j = 0; j < SIZE; j++)
+            {
+                result.setAt(i, j, this.getAt(i, j) + other.getAt(i, j));
+            }
+        }
+        return result;
+    }
+}
--- src/math/Vector4.java
+++ src/math/Vector4.java
@@ -3,89 +3,110 @@ package math;
 /**
  * Описывает трехмерную точку в виде вектора из четырех компонент для матричной алгебры
  */
-public class Vector4
-{
+public class Vector4 {
     private float[] values;
+    public static final int SIZE = 4;
 
     /**
      * Конструктор трехмерной точки по 4-м составляющим
+     *
      * @param x
      * @param y
      * @param z
      * @param w
      */
-    public Vector4(float x, float y, float z, float w)
-    {
+    public Vector4(float x, float y, float z, float w) {
         this.values = new float[]{x, y, z, w};
     }
 
     /**
      * Конструктор трехмерной точки по 3-м составляющим
+     *
      * @param x
      * @param y
      * @param z
      */
-    public Vector4(float x, float y, float z)
-    {
+    public Vector4(float x, float y, float z) {
         this.values = new float[]{x, y, z, 0};
     }
 
     /**
      * Конструктор трехмерной точки по 4-м составляющим
+     *
      * @param vector {@link Vector3}
      * @param w
      */
-    public Vector4(Vector3 vector, float w)
-    {
+    public Vector4(Vector3 vector, float w) {
         this.values = new float[]{vector.getX(), vector.getY(), vector.getZ(), w};
     }
 
     /**
      * Конструктор трехмерной точки по 3-м составляющим
+     *
      * @param vector {@link Vector3}
      */
-    public Vector4(Vector3 vector)
-    {
+    public Vector4(Vector3 vector) {
         this.values = new float[]{vector.getX(), vector.getY(), vector.getZ(), 0};
     }
 
-    private static final float EPS = 1e-12f;
-    public Vector4 normalized()
-    {
-        if (Math.abs(getW()) < EPS)
-        {
-            return new Vector4(getX(), getY(), getZ(), 0);
-        }
-        return new Vector4(getX()/getW(), getY()/getW(), getZ()/getW(), 1f);
+    public Vector4(float[] values) {
+        this.values = values;
     }
 
-    public float getX()
-    {
+    private static final float EPS = 1e-12f;
+
+    public Vector4 normalized() {
+        if (Math.abs(getW()) < EPS) {
+            return new Vector4(getX(), getY(), getZ(), 0);
+        }
+        return new Vector4(getX() / getW(), getY() / getW(), getZ() / getW(), 1f);
+    }
+
+    public Vector4 multiply(float number) {
+        float[] result = new float[SIZE];
+        for (int i = 0; i < SIZE; i++)
+        {
+            result[i] = this.at(i) * number;
+        }
+        return new Vector4(result);
+    }
+
+    public Vector4 add(Vector4 other) {
+        float[] result = new float[SIZE];
+        for (int i = 0; i < SIZE; i++)
+        {
+            result[i] = this.at(i) + other.at(i);
+        }
+        return new Vector4(result);
+    }
+
+    public static Vector4 zero() {
+        return new Vector4(new float[SIZE]);
+    }
+
+    public float getX() {
         return values[0];
     }
 
-    public float getY()
-    {
+    public float getY() {
         return values[1];
     }
 
-    public float getZ()
-    {
+    public float getZ() {
         return values[2];
     }
 
-    public float getW()
-    {
+    public float getW() {
         return values[3];
     }
 
     /**
      * Возвращает значение компоненты по индексу
+     *
      * @param index
      * @return
      */
-    public float at(int index)
-    {
+    public float at(int index) {
         return values[index];
     }
 }
